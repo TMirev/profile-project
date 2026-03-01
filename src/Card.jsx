1999a27fab15e706234e5eee901fc3a09f967f5f
Tenko Mirev <t.d.mirev@gmail.com>
2026-03-01 09:02:24 +0000

    Changes over About me section

--- src/Card.jsx
+++ src/Card.jsx
@@ -53,9 +53,16 @@ function Card({ children }) {
             <div className="contacts-summary-box">
                 <h2>About me</h2>
 
+                <h3>Summary</h3>
                 <p>Motivated Front-End Developer focused on creating reponsive, accesible and visually appealing web interfaces.</p>
+
+                <h3>Technical Skills</h3>
                 <p>Skilled in HTML, CSS, JavaScript and React with hands on experience building personal projects such as Profile Page, Notes Board, Travel Website and Digital Watch project</p>
+
+                <h3>Approach and Practice</h3>
                 <p>Combines clean code practices with a growing understanding of UI/UX principles to deliver easy to navigate user experience. Comfortable working with modern tools including React, GitHub, VsCode and CSS techniques including Flexbox, Grid and animations.</p>
+
+                <h3>Values and Goals</h3>
                 <p>Passionate about improving performance, writing maintainable code and building projects. Detail-oriented, I care about clarity, simplicity and creating interfaces that feel great to use. Commited to ongoing growth in Front-end development with a long-term role of becoming well-rounded software developer.</p>
             </div>
 
